4ad3d22a701df23194fe41e31cc632b5b4719925
jadx2 <delmarjaim@gmail.com>
2021-07-02 14:32:29 -0500

    Refactor App component from stateful class to stateful function with hooks

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -14,7 +14,7 @@ const calculate = (data, buttonName) => {
       if (!total) {
         return {};
       }
-      if (total && !operation && !next) {
+      if (total && (!operation || operation === '=') && !next) {
         total = (total * -1).toString();
       } else if (next) {
         next = (next * -1).toString();
@@ -72,7 +72,12 @@ const calculate = (data, buttonName) => {
       if (!total) {
         total = '0.';
       } else if (total && operation && !next) {
-        next = '0.';
+        if (operation === '=') {
+          total += '.';
+          operation = null;
+        } else {
+          next = '0.';
+        }
       } else if (!operation && !total.includes('.')) {
         total += '.';
       } else if (operation && !next.includes('.')) {
